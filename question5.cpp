ffa6aac80993b09f9bcdbe612cc9bf0474866e96
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-23 23:35:08 +0530

    Create question5.cpp

---
+++ question5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    	float l, b;
+    
+	cout <<"enter the length and breadth of a rectangle"<<endl;
+	cin >> l;
+	cin >> b;
+	cout << "length = "<<l<<endl;
+	cout << "breadth = "<<b<<endl;
+	cout << "area of the rectangle = " << l*b ;
+
+    return 0;
+}
