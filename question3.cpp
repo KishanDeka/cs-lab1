a72960da0dba126e233fdc23731524d6ce0e59c6
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-23 23:46:21 +0530

    Create question3.cpp

---
+++ question3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    	int a, b;
+	cout <<"enter two number"<<endl;
+	cin >> a;
+	cin >> b;
+            cout << "num1 = "<< a<<endl;
+            cout << "num2 = "<< b<<endl;
+           	cout << "num1 + num2 = " << a+b <<endl ;
+            cout << "num1 - num2 = " << a-b <<endl  ;
+            cout << "num1*num2 = " << a*b <<endl ;
+            cout << "num1/num2 = " << a/b << endl ;
+    return 0;
+}
