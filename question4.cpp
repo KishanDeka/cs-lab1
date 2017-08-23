03f960c324c4ec9ff6a230894d9b955ab3da2b6a
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-23 23:25:57 +0530

    Create question4.cpp

---
+++ question4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    	int l, b;
+    
+	cout <<"enter the length and breadth of a rectangle"<<endl;
+	cin >> l;
+	cin >> b;
+	cout << "length = "<< l<<endl;
+	cout << "breadth = "<< b<<endl;
+	cout << "perimeter of the rectangle = " << 2*(l+b) ;
+
+    return 0;
+}
