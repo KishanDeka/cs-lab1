76a407ac918c25624ef89c46fce76e0f3f5ee3ec
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-23 23:32:10 +0530

    Create question2.cpp

---
+++ question2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    	int a, b;
+    
+	cout <<"enter two integer number"<<endl;
+	
+	cin >> a;
+	
+	cin >> b;
+             cout << "num1 = "<<a<<endl;
+             cout << "num2 = "<<b<<endl;
+	cout << "num1 + num2 = " << a+b ;
+
+    return 0;
+}
