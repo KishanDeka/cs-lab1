20aa77fa284090eee10abb46c8be9cd05fd5e94c
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-23 23:28:08 +0530

    Create question1.cpp

---
+++ question1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a, b, c;
+    int x=18;
+    float y=1.3;
+    char z ='m';
+    bool f= true;
+    cout << "age = "<< x<<endl<< "hight = " << y<<endl<< "sex - " <<z<<endl<< f;
+	return 0;
+}
