566e642765aca69091c81822286d076b68d1f832
KishanDeka <31157122+KishanDeka@users.noreply.github.com>
2017-08-24 20:18:27 +0530

    Update question3.cpp

--- question3.cpp
+++ question3.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 int main()
 {
-    	int a, b;
+    	float a, b;
 	cout <<"enter two number"<<endl;
 	cin >> a;
 	cin >> b;
             cout << "num1 = "<< a<<endl;
             cout << "num2 = "<< b<<endl;
-           	cout << "num1 + num2 = " << a+b <<endl ;
+            cout << "num1 + num2 = " << a+b <<endl ;
             cout << "num1 - num2 = " << a-b <<endl  ;
             cout << "num1*num2 = " << a*b <<endl ;
             cout << "num1/num2 = " << a/b << endl ;
